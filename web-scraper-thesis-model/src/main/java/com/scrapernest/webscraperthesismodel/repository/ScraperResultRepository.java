ca966baf4dfd48e51d4ecba0ea0e3ba6fef2a8a4
Shukrah-036 <adekumbi036@gmail.com>
2024-03-19 00:19:02 +0100

    Updating the ScraperResultRepository file

--- web-scraper-thesis-model/src/main/java/com/scrapernest/webscraperthesismodel/repository/ScraperResultRepository.java
+++ web-scraper-thesis-model/src/main/java/com/scrapernest/webscraperthesismodel/repository/ScraperResultRepository.java
@@ -4,6 +4,10 @@ import com.scrapernest.webscraperthesismodel.model.ScraperResult;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ScraperResultRepository extends MongoRepository<ScraperResult, String> {
+
+    List<ScraperResult> findByScraperName(String scraperName);
 }
