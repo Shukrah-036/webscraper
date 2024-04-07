8963f9e1994270bbd85db553ca13cb39713654f3
Shukrah-036 <adekumbi036@gmail.com>
2024-04-07 14:15:43 +0200

    Updating the UserRepository class

--- web-scraper-thesis-model/src/main/java/com/scrapernest/webscraperthesismodel/repository/UserRepository.java
+++ web-scraper-thesis-model/src/main/java/com/scrapernest/webscraperthesismodel/repository/UserRepository.java
@@ -4,7 +4,17 @@ import com.scrapernest.webscraperthesismodel.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
+
+    boolean existsByUsername(String username);
+
+    boolean existsByEmail(String email);
+
+    User findByUsername(String username);
+
+    Optional<User> findByEmail(String email);
 }
 
